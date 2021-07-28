bc86b80d377435c08475e8417c327faabfa47a4c
Manish Gupta <manish.gta1@gmail.com>
2021-07-28 12:29:13 +0530

    Bundled multiple files: utils.js and app.js. As login.js required in app.js so it will also bundle in the bundle.js file

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// CommonJS syntax to bring login js file into app file. 
+// So login file execute first because it is at top then app.js console log execute after that
+require('./login'); 
+
 document.write("Welcome to Big Hair Concerts!!");
 
 console.log('App loaded');
