d5a21cfe338fdb8d88eea630a568bb94e4699b2e
Manish Gupta <manish.gta1@gmail.com>
2021-07-28 15:27:37 +0530

    adding source map support

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@
 // So login file execute first because it is at top then app.js console log execute after that
 require('./login'); 
 
-document.write("Welcome to Big Hair Concerts!");
+document.write("Welcome to Big Hair Concerts!!");
 
 console.log('App loaded');
