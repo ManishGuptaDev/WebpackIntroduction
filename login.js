4e471267de26e0afa4fcc6de3fffbe3256032bb4
Manish Gupta <manish.gta1@gmail.com>
2021-07-28 13:46:45 +0530

    Added babel loader to convert ES6 files into es2015 so that application can run on old browser also We have seen how to use loader here

--- login.js
+++ login.js
@@ -1,1 +1,7 @@
-console.log('login loaded');
+let login = (username, password) => {
+	if(username !== 'admin'  || password !== 'radical') {
+		console.log('incorrect login');
+	}
+};
+
+login('admin', 'idunno');
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,14 @@ module.exports = {
     entry: ["./utils.js","./app.js"],
     output: {
         filename: "dist/bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /login\.js/,
+                exclude: /node_modules/,
+				loader: "babel-loader"
+            }
+        ]
     }
 }
